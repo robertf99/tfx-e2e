872cf45c70d2e28bc4ca10f6fe2a7c57e46e2d77
robertf99 <robertf99@gmail.com>
2021-08-23 21:10:55 +1000

    add kubeflow runner

---
+++ penguin-pipeline/kubeflow_e2e_runner.py
@@ -0,0 +1,38 @@
+from tfx import v1 as tfx
+from pipeline.config import pipe_config
+from pipeline.e2e_pipeline.pipeline import create_pipeline
+
+
+def run():
+    """Define a pipeline to be executed using Kubeflow V2 runner."""
+
+    tfx_image = pipe_config.TFX_IMAGE
+    metadata_config = (
+        tfx.orchestration.experimental.get_default_kubeflow_metadata_config()
+    )
+
+    runner_config = tfx.orchestration.experimental.KubeflowDagRunnerConfig(
+        kubeflow_metadata_config=metadata_config, tfx_image=tfx_image
+    )
+    pod_labels = {
+        "add-pod-env": "true",
+        tfx.orchestration.experimental.LABEL_KFP_SDK_ENV: "tfx-template",
+    }
+
+    dsl_pipeline = create_pipeline(
+        pipeline_name=pipe_config.PIPELINE_NAME,
+        pipeline_root=pipe_config.PIPELINE_ROOT,
+        data_root=pipe_config.DATA_ROOT,
+        schema_path=pipe_config.SAVED_SCHEMA_PATH,
+        trainer_module_file=pipe_config.TRAINER_MODULE_PATH,
+        serving_model_dir=pipe_config.SERVING_MODEL_DIR,
+    )
+    runner = tfx.orchestration.experimental.KubeflowDagRunner(
+        config=runner_config, pod_labels_to_attach=pod_labels
+    )
+
+    runner.run(pipeline=dsl_pipeline)
+
+
+if __name__ == "__main__":
+    run()
